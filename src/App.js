4f99253835ab0e89238201fd67e5b6b3e4d79a9f
David Grande Troncoso <david_grande_95@hotmail.com>
2022-08-17 18:01:57 +0200

    Default Search "React"

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
     },
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState('');
+  const [searchTerm, setSearchTerm] = React.useState('React');
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
@@ -32,7 +32,7 @@ const App = () => {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search onSearch={handleSearch} />
+      <Search search={searchTerm} onSearch={handleSearch} />
 
       <hr />
 
@@ -41,12 +41,12 @@ const App = () => {
   );
 };
 
-const Search = (props) => {
+const Search = (props, searchTerm) => {
 
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={props.onSearch} />
+      <input id="search" type="text" value={props.search} onChange={props.onSearch} />
     </div>
   );
 };
